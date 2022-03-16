bd2e974224346d07df8b792afc492021569174dc
nikolinatomasevic <nikolina.tomasevic2000@gmail.com>
2022-03-16 18:12:23 +0100

    Implementirana klasa Magacin na master grani

--- src/magacin/Magacin.java
+++ src/magacin/Magacin.java
@@ -1,25 +1,50 @@
 package magacin;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import magacin.interfejs.MagacinInterfejs;
 
 public class Magacin implements MagacinInterfejs {
 
+	List<Artikal> artikli;
+	
+	public Magacin() {
+		artikli = new ArrayList<Artikal>();
+	}
+	
 	@Override
 	public void dodajArtikal(Artikal artikal) {
-		// TODO Auto-generated method stub
+		if(artikli.contains(artikal)) {
+			for(Artikal a: artikli) {
+				if(a.equals(artikal)) {
+					a.setKolicina(a.getKolicina() + artikal.getKolicina());
+				}
+			}
+		}else {
+			artikli.add(artikal);
+		}
 
 	}
 
 	@Override
 	public void izbaciArtikal(Artikal artikal) {
-		// TODO Auto-generated method stub
+		if(!artikli.contains(artikal)) {
+			throw new RuntimeException("Artikal ne postoji");
+		}
+		Artikal a = pronadjiArtikal(artikal.getSifra());
+		a.setKolicina(a.getKolicina() - artikal.getKolicina());
 
 	}
 
 	@Override
 	public Artikal pronadjiArtikal(int sifra) {
-		// TODO Auto-generated method stub
-		return null;
+		for(Artikal a: artikli) {
+			if(a.getSifra() == sifra) {
+				return a;
+			}
+		}
+		throw new RuntimeException("Ne postoji artikal sa zadatom sifrom");
 	}
 
 }
