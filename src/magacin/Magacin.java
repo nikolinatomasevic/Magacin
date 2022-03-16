cfd3f8e548129692953b26f1b520b471f3a666ab
nikolinatomasevic <nikolina.tomasevic2000@gmail.com>
2022-03-16 17:52:32 +0100

    Napravljena klasa Magacin koja implementira interfejs MagacinInterfejs

---
+++ src/magacin/Magacin.java
@@ -0,0 +1,25 @@
+package magacin;
+
+import magacin.interfejs.MagacinInterfejs;
+
+public class Magacin implements MagacinInterfejs {
+
+	@Override
+	public void dodajArtikal(Artikal artikal) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void izbaciArtikal(Artikal artikal) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Artikal pronadjiArtikal(int sifra) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
