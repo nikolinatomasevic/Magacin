a7ca34ba308d33926906f11691d08568cd67af94
nikolinatomasevic <nikolina.tomasevic2000@gmail.com>
2022-03-16 17:28:23 +0100

    Dodati atributi i get i set metode u klase Artikal i KucnaHemija

--- src/magacin/Artikal.java
+++ src/magacin/Artikal.java
@@ -2,4 +2,41 @@ package magacin;
 
 public class Artikal {
 
+	String naziv;
+	int sifra;
+	String opis;
+	int kolicina;
+	
+	public String getNaziv() {
+		return naziv;
+	}
+	
+	public int getSifra() {
+		return sifra;
+	}
+	
+	public String getOpis() {
+		return opis;
+	}
+	
+	public int getKolicina() {
+		return kolicina;
+	}
+	
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+	
+	public void setSifra(int sifra) {
+		this.sifra = sifra;
+	}
+	
+	public void setOpis(String opis) {
+		this.opis = opis;
+	}
+	
+	public void setKolicina(int kolicina) {
+		this.kolicina = kolicina;
+	}	
+	
 }
--- src/magacin/KucnaHemija.java
+++ src/magacin/KucnaHemija.java
@@ -1,5 +1,17 @@
 package magacin;
 
+import java.util.Date;
+
 public class KucnaHemija {
 
+	Date rokTrajanja;
+
+	public Date getRokTrajanja() {
+		return rokTrajanja;
+	}
+
+	public void setRokTrajanja(Date rokTrajanja) {
+		this.rokTrajanja = rokTrajanja;
+	}
+	
 }
